60daa3d606c5e2ebe85460c6dd3726ca3598f560
Fleming John <flemjohn08@gmail.com>
2025-06-16 23:30:49 +0530

    Added Dictionary and its project

--- Python-Tutorial/03_DataStructures/03_dictionaries.py
+++ Python-Tutorial/03_DataStructures/03_dictionaries.py
@@ -1,1 +1,58 @@
+# Dictionary = a collection of {key:value} pairs
+# Ordered and changeable. No duplicates
 
+capitals = {
+
+    "usa" : "Dc",
+    "india" : "new delhi",
+    "china" : "beijing",
+    "russia" : "moscow"
+}
+
+
+# print(dir(capitals))
+print(help(capitals))
+
+# geting the values of the dict  
+print(capitals.get("india"))
+
+if capitals.get("Russia"):
+    print("The capital exist")
+
+else:
+    print("That capital exist")
+
+# updating the dict 
+capitals.update({"Germany": "Berlin"})
+capitals.update({"USA":"detroit"})
+print(capitals)
+
+# Removing the dict
+capitals.pop("china")
+capitals.popitem()
+print(capitals)
+
+# print keys only
+keys =  capitals.keys()
+print(keys)
+
+# printing keys using loop
+for key in capitals.keys():
+    print(key)
+
+# print values 
+values = capitals.values()
+print(values)
+
+# printing values using loop
+for value in capitals.values():
+    print(value)
+
+# getting the items both key and value in dict
+items = capitals.items()
+print(items,"<-ITEMS")
+
+# getting both items in loop 
+for key, value in capitals.items():
+    print("Key->",key)
+    print("Value ->",value)
--- Python-Tutorial/10_Projects/concession_stand.py
+++ Python-Tutorial/10_Projects/concession_stand.py
@@ -1,1 +1,45 @@
+# Concesion is nothing but menu items 
 
+# menu -> collection of food items
+menu = {
+    "dosa": 30,
+    "idly":40,
+    "poori":60,
+    "rosemilk":89,
+    "badammilk":90
+}
+
+# cart to store the buyed foods
+cart = []
+
+# total represents the no of food
+total = 0
+
+print("---MENU---")
+for key, value in menu.items():
+    print(f"{key:10}:{value} ")
+
+# Decorator
+print("----------")
+
+while True:
+    food =  input("Select an item(q to quit):").lower()
+    if food == "q":
+        break
+
+    elif menu.get(food) is not None:
+        cart.append(food)
+    
+    else:
+        print("Items not available")
+
+# print your orders after adding to the cart 
+print("---Your Order ---")
+
+
+for food in cart:
+    total += menu.get(food)
+    print(f"Food: {food}")
+
+print() #adding space to the output
+print("Total of your food is: ",total)
