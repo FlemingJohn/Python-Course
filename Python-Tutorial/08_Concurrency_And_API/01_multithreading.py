7b089bd4e44d7c833ef72b398c73cea9d701c43a
Fleming John <flemjohn08@gmail.com>
2025-06-20 23:57:01 +0530

    Added Concurrency and api

--- Python-Tutorial/08_Concurrency_And_API/01_multithreading.py
+++ Python-Tutorial/08_Concurrency_And_API/01_multithreading.py
@@ -1,1 +1,34 @@
+# Multithreading = Used to perform multiple tasks concurrently (multitasking)
+# Good for I/O bound tasks like reading files or fetching data from APIs
 
+import threading
+import time 
+
+def walk_dog(first,last):
+    time.sleep(1)
+    print(f"You finish walking {first} {last}")
+
+def take_out_trash():
+    time.sleep(2)
+    print("you take out the trash")
+
+def get_mail():
+    time.sleep(4)
+    print("you get the mail")
+
+# create threads for each chore
+chore1 = threading.Thread(target = walk_dog, args=("Scooby","Doo"))
+chore2 = threading.Thread(target=take_out_trash)
+chore3 = threading.Thread(target=get_mail)
+
+# start threads
+chore1.start()
+chore2.start()
+chore3.start()
+
+# join()function ensure that all tasks are completed before proceeding 
+chore1.join()
+chore2.join()
+chore3.join()
+
+print("All chores are completed")
