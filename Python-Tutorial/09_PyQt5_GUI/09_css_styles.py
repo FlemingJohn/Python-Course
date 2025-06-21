f0294578f31ea7e89bc465c70aa16e0bd33e94e7
Fleming John <flemjohn08@gmail.com>
2025-06-21 23:38:06 +0530

    Added files

--- Python-Tutorial/09_PyQt5_GUI/09_css_styles.py
+++ Python-Tutorial/09_PyQt5_GUI/09_css_styles.py
@@ -1,1 +1,17 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QWidget, QHBoxLayout    # type: ignore
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.button1 = QPushButton("#1")
+        self.button2 = QPushButton("#2")
+        self.button3 = QPushButton("#3")
+        self.initUI()
+
+    def initUI(self):
+        central_widget = Qwidget() #type: ignore
+        self.setCentralWidget(central_widget)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.button1)
