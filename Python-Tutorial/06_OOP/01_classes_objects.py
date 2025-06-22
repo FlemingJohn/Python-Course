75bacb14587dfbae5b1fb2116a5ee53913218d67
Fleming John <flemjohn08@gmail.com>
2025-06-22 22:43:42 +0530

    Added solution

--- Python-Tutorial/06_OOP/01_classes_objects.py
+++ Python-Tutorial/06_OOP/01_classes_objects.py
@@ -1,1 +1,34 @@
+# object = A "bundle" of related attributes (variables) and methods (functions)
+# Ex: phone, cup, book
+# You need a "class" to create many objects
 
+# class  = (blueprint) used to design the structure and layout of an object
+
+class Car:
+    def __init__(self,model,year,color,for_sale):
+        self.model = model 
+        self.year = year
+        self.color = color
+        self.for_sale = for_sale
+
+    def drive(self):
+        print("You drive the car")
+        print(f"You drive the {self.model}")
+        print(f"You drive the {self.color} {self.model}")
+
+    def stop(self):
+        print("You stop the car")
+        print(f"You stop the {self.model}")
+        print({f"You stop the {self.color} {self.model}"})
+
+    def describe(self):
+        print(f"{self.year} {self.color} {self.model}")
+
+car1 = Car("Mustan",2024,"red",False)
+car2 = Car("ford",2025,"red",True)
+car3 = Car("Benz",2026,"red",False)
+
+print(car1.model)
+print(car1.year)
+print(car1.color)
+print(car2.for_sale)
--- Python-Tutorial/06_OOP/02_class_variables.py
+++ Python-Tutorial/06_OOP/02_class_variables.py
@@ -1,1 +1,20 @@
+# Class variable = shared among all instances of a class defined outside the constructor.
+# allow you to share data among all objects created from that class 
 
+class Student:
+    class_year = 2025
+    num_students = 0
+
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age 
+        Student.num_students += 1
+
+student1 = Student("Spongebob", 30)
+student2 = Student("Patrick",35)
+student3 = Student("Fleming",21)
+
+print(f"My graduating class of {Student.class_year} has {Student.num_students} students")
+print(student1.name)
+print(student2.name)
+print(student3.name)
